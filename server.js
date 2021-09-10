63d2d7d78c39a9943116e718fb293dc3750d49b6
Claudio Saillant <claudiojsaillant@gmail.com>
2021-09-10 15:05:43 -0400

    react proxy set

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require("path")
+
 let app = express();
-const PORT = 8080 // Goes inside .env; hardcoded for testing purposes.
+const PORT = process.env.PORT || 5000 // Goes inside .env; hardcoded for testing purposes.
 
 // simple route for testing
 
-app.get('/', (req, res) => {
-    res.send("Working route");
+app.get('/backend', (req, res) => {
+    res.send("Express server working")
+    
 })
+
 
 app.listen(PORT, () => {
     console.log("Server successfully listening at: http://localhost:" + PORT);
